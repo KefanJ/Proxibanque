ab084f6996a78062d8cf9004fae83c61dba54873
KefanJ <kefanj10@gmail.com>
2022-12-21 12:35:42 +0100

    ajout cross origin

--- src/main/java/com/proxibanque/server/controller/RestCompteController.java
+++ src/main/java/com/proxibanque/server/controller/RestCompteController.java
@@ -1,6 +1,7 @@
 package com.proxibanque.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.proxibanque.server.entity.Client;
 import com.proxibanque.server.entity.Compte;
 import com.proxibanque.server.service.CompteBancaireService;
 
 @RestController
 @RequestMapping("/compte")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class RestCompteController {
 
 	@Autowired
--- src/main/java/com/proxibanque/server/controller/RestConseillerController.java
+++ src/main/java/com/proxibanque/server/controller/RestConseillerController.java
@@ -3,6 +3,7 @@ package com.proxibanque.server.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.proxibanque.server.service.ConseillerService;
 
 @RestController
 @RequestMapping("/conseillers")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class RestConseillerController {
 	@Autowired
 	ConseillerService service;
